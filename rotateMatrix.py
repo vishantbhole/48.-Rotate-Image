c0ee3050ec7623c82adb5b5032cc3e1724f7c83f
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-07 21:47:48 -0600

    Add test case for rotateMatrix function

--- rotateMatrix.py
+++ rotateMatrix.py
@@ -25,3 +25,6 @@ if __name__ == "__main__":
     sol = Solution()
     matrix = [[1,2,3],[4,5,6],[7,8,9]]
     print("Output is : ", sol.rotateMatrix(matrix))
+    
+    matrix2 = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+    print("Output is : ", sol.rotateMatrix(matrix2))
