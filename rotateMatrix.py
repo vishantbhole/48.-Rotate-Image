4d5abb5efdc5a9156308d97e98b0499f6cfba0a9
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-07 21:45:19 -0600

    Implement rotateMatrix method in Solution class
    Added a class Solution with a method to rotate a matrix in-place.

--- rotateMatrix.py
+++ rotateMatrix.py
@@ -1,2 +1,10 @@
 #48. Rotate Image
 
+from typing import List
+
+
+class Solution:
+    def rotateMatrix(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
