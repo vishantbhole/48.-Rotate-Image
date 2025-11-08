0146b556104109432fd14f953fb03c273cebf796
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-07 21:46:55 -0600

    Fix matrix rotation logic in rotateMatrix.py

--- rotateMatrix.py
+++ rotateMatrix.py
@@ -15,3 +15,9 @@ class Solution:
                 top, bottom = left, right
                 topLeft = matrix[top][left + i]
                 matrix[top][left + i] = matrix[bottom - i][left]
+                matrix[bottom - i][left] = matrix[bottom][right - i]
+                matrix[bottom][right - i] = matrix[top + i][right]
+                matrix[top + i][right] = topLeft
+            left += 1
+            right -= 1
+        return matrix
